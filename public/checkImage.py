c590414fc020b489fd4c79919719b7d74c2e3a57
aldonesia <rizkyfenaldoo@gmail.com>
2021-05-01 00:03:14 +0700

    add dimension cv2

--- public/checkImage.py
+++ public/checkImage.py
@@ -6,13 +6,14 @@ import sys
 if __name__=="__main__":
    
     nmFile=sys.argv[1]
-    print(nmFile)
     dim=96
     img = cv2.imread(nmFile)
     if img is None:
         os.system('del %s'%nmFile)
         print("REJECTED, not image remove , no add to repository  ")
     else:
+        print(nmFile)
+        print('height, width, color:', img.shape)
         print("ACCEPTED")
 
         
